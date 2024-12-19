1c6b5e356ea828afd4835e07f695054448105840
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-19 11:58:02 +0530

    fixed; Homepage ui Final

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function page() {
   
   return (
     <div className="min-h-[100vh] w-[100vw] bg-secondary dark:bg-black flex flex-col items-center ">
-      <div className="sm:w-[67vw] max-sm:w-[90vw]  min-h-80 flex flex-col items-start justify-around mt-4">
-        <h1 className="font-inter text-4xl pb-4 font-extralight max-sm:text-2xl">Gyanaranjan Patra</h1>
-        <p className="font-inter font-thin text-lg text-gray-800 dark:text-white">
+      <div className="sm:w-[67vw] max-sm:w-[90vw]  min-h-80 flex flex-col items-start justify-around sm:mt-4 max-sm:mt-10">
+        <h1 className="font-inter text-4xl pb-4 font-extralight max-sm:text-3xl">Gyanaranjan Patra</h1>
+        <p className="font-inter  text-lg text-gray-800 dark:text-white">
         Hello! I am a budding Full-Stack Web Developer and a second-year IT student with a keen interest in AI and Blockchain technologies. With hands-on experience in the MERN stack, I specialize in developing efficient and user-friendly web applications. I am committed to continuous learning and exploration of cutting-edge technologies to create impactful AI and blockchain solutions that enhance user experiences and system performance. I am available to dedicate 25-30 hours per week, ensuring flexibility and effective collaboration in any time zone.
         </p>
         <Link className="px-6 py-3 rounded-md bg-primary text-secondary dark:bg-white dark:text-black font-bold max-sm:mt-5" href="/about">About Me</Link>
