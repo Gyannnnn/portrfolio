c4a4cfac94a9aa397c19b1a38c84dac9fd5cd2ac
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-21 19:04:59 +0530

    Fixed; Bugs

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Resume() {
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center justify-center mt-16'>
         <iframe className='w-[100vw] h-screen' src="/resume/resume.pdf" ></iframe>
     </div>
   )
