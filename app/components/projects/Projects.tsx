2501e6b46f0da973f4ced403e7ef9e9fb7457926
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-21 17:55:02 +0530

    Fixed; Bugs

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -12,25 +12,23 @@ export default function Projects() {
       <h1 className="font-inter font-extrabold text-4xl">Projects</h1>
       <div className="flex max-sm:flex-col justify-center items-center gap-4">
         {data.slice(0, 2).map((project, index) => (
-          <Link href="/projects" key={index}>
-            <Card className="sm:w-[33vw] max-sm:w-[90vw] flex flex-col gap-4 border h-[480px] rounded-xl">
-              <div className="h-[250px] w-full overflow-hidden">
-                <Image
-                  src={project.logo}
-                  width={500}
-                  height={500}
-                  alt="Project Image"
-                  className="rounded-t-lg filter grayscale-0 hover:grayscale object-cover h-full w-full"
-                />
-              </div>
-              <div className="flex flex-col justify-between flex-1 p-4">
-                <h1 className="font-inter text-3xl truncate">{project.title}</h1>
-                <p className="text-sm text-gray-700 dark:text-white line-clamp-6">
-                  {project.description}
-                </p>
-              </div>
-            </Card>
-          </Link>
+          <Card className="sm:w-[33vw] max-sm:w-[90vw] flex flex-col gap-4 border h-[480px] rounded-xl">
+            <div className="h-[250px] w-full overflow-hidden">
+              <Image
+                src={project.logo}
+                width={500}
+                height={500}
+                alt="Project Image"
+                className="rounded-t-lg filter grayscale-0 hover:grayscale object-cover h-full w-full"
+              />
+            </div>
+            <div className="flex flex-col justify-between flex-1 p-4">
+              <h1 className="font-inter text-3xl truncate">{project.title}</h1>
+              <p className="text-sm text-gray-700 dark:text-white line-clamp-6">
+                {project.description}
+              </p>
+            </div>
+          </Card>
         ))}
       </div>
       <Link
